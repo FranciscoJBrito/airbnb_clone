29dca70d62f97dc9f7591f1950114f2591fff82f
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-04-10 15:46:39 -0300

    adding index to reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_04_144725) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_10_183604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_04_144725) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["property_id"], name: "index_reservations_on_property_id"
+    t.index ["user_id", "property_id", "checking_date", "checkout_date"], name: "add_index_to_reservations", unique: true
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
